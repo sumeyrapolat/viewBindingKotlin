2b59994266b6dce039fb86f713cdefbeee7e6ea1
sumyera <180315055@ogr.cbu.edu.tr>
2023-12-11 13:28:22 +0300

    fist commit

---
+++ app/src/main/java/com/sumeyra/viewbindingkotlin/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.sumeyra.viewbindingkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sumeyra.viewbindingkotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+
+        }
+
+        binding.textView.text = "Test"
+
+        binding.editText.setText("EditText Text")
+    }
+
+
+}
